40e5089bc6fe141305fc9b0e07c54776f1aaeb9a
pinzap_ <151607984+judSoftwareDev@users.noreply.github.com>
2025-09-18 15:32:55 +0800

    Create val.kt

---
+++ VAL&VAR/val.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val x = 24
+
+    //int data type syntax
+    val y : Int = 6
+
+    //float data type syntax
+    val z : Float = 5f
+
+    //Boolean data type syntax
+    val a : Boolean = true
+
+    //String data type syntax
+    val b : String = "Jud Pinza"
+
+    println("Hello World from kotlin!")
+    println(x)
+    println("Int Data Type: " + y)
+    println("Float Data Type: " + z)
+    println("Boolean Data Type: " + a)
+    println("This is String Data Type: " + b)
+}

920c8c6c4d975fe0676c6ba9002906d6a81676c9
pinzap_ <151607984+judSoftwareDev@users.noreply.github.com>
2025-09-18 15:50:56 +0800

    Create var.kt

---
+++ VAL&VAR/var.kt
@@ -0,0 +1,6 @@
+fun main(){
+    //The value of a var variable can be reassigned multiple times after its initial declaration.
+    var x = "This is the original text!!"
+    x = "Updated original text!"
+    println(x)
+}
